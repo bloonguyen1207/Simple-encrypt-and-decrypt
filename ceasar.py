ba9ed82aff42f0773680a02dd96889b59348c1b3
Bloo Nguyen <bloonguyen1207@gmail.com>
2016-12-01 18:11:50 +0700

    Code comment

--- ceasar.py
+++ ceasar.py
@@ -1,46 +1,64 @@
 import sys
 
 
+# Encrypt
 def encrypt(file, key):
+    # It cannot read /n properly so I don't use it
     # with open('alphabet.txt', 'r') as af:
     #     alpha = af.read().rstrip()
+
+    # Alphabet
     alpha = """ABCDEFGHIJKLMNOPQRSTUVWXYZ .,:;()-!?$'"\n0123456789"""
+
+    # Open file and read whole file then strip newline (if any)
+    # then strip '<>' characters
     with open(file, 'r') as f:
         message = f.read().rstrip().strip('<>')
+
+    # Make sure message is uppercase so that it can be mapped with alphabet
     message = message.upper()
+
     message_enc = ""
-    for i in range(len(message)):
-        if message[i] in alpha:
-            j = alpha.find(message[i])
-            message_enc += alpha[(j + int(key)) % len(alpha)]
+    # Loop through each character in message and check with alphabet
+    for c in range(len(message)):
+        # If character is in alphabet, get the index of that character in alphabet
+        if message[c] in alpha:
+            index = alpha.find(message[c])
+
+            # Get sum of character index with key then mod the length of alphabet
+            # Then add the character at the computed index to the encrypted message
+            message_enc += alpha[(index + int(key)) % len(alpha)]
         else:
-            raise Exception("Not in alphabet")
+            print(message[c] + " is not in alphabet.")
 
     return message_enc
 
 
+# Undo the encryption to decrypt
 def decrypt(file, key):
     return encrypt(file, -key)
 
 
 mode = sys.argv[1]
 filename = sys.argv[2]
-key = sys.argv[3]
+key_ar = sys.argv[3]
 
-if mode == 'e':
-    print(encrypt(filename, key))
-elif mode == 'd':
-    if key == "unknown":
+if mode == 'e':     # Encrypt
+    print(encrypt(filename, key_ar))
+elif mode == 'd':   # Decrypt
+    # Try all keys if the key is unknown - input unknown for unknown key
+    if key_ar == "unknown":
         for i in range(1, 50):
-            key = i
+            key_ar = i
             print("----------------------------------------------")
-            print("Key: " + str(key))
+            print("Key: " + str(key_ar))
             print("----------------------------------------------")
-            print(decrypt(filename, int(key)))
+            print(decrypt(filename, int(key_ar)))
             print("----------------------------------------------")
-    elif key.isdigit():
-        print(decrypt(filename, int(key)))
+    # Key validation
+    elif key_ar.isdigit():
+        print(decrypt(filename, int(key_ar)))
     else:
         print("Invalid key. Please try again.")
-else:
+else:               # Mode validation
     print("Invalid input.")
--- subts.py
+++ subts.py
@@ -3,6 +3,7 @@ import random
 import json
 
 
+# Check frequency of each character in the message - Can be used to guess key
 def checkfrequency(file):
     alphabet = """ABCDEFGHIJKLMNOPQRSTUVWXYZ .,:;()-!?$'"\n0123456789"""
     frequency = {}
@@ -13,11 +14,16 @@ def checkfrequency(file):
     return frequency
 
 
+# Generate key file
 def genkey(file):
     alphabet = list("""ABCDEFGHIJKLMNOPQRSTUVWXYZ .,:;()-!?$'"\n0123456789""")
     alphabet_cp = alphabet.copy()
     key = {}
 
+    # Get random number within alphabet length
+    # Map the character with character at random index
+    # Remove mapped characters
+    # Keep going until we a full dictionary with key and values
     for i in range(len(alphabet)):
         index = random.randint(0, len(alphabet_cp) - 1)
         key[alphabet[i]] = alphabet_cp[index]
@@ -27,43 +33,65 @@ def genkey(file):
         json.dump(key, key_file)
 
 
+# Keyfile must be in dictionary format
 def encrypt(file, key_file):
+    # Load key as dictionary
     key = json.load(open(key_file, 'r'))
+
+    # Read message from file and remove '<>' characters
     with open(file, 'r') as f:
         message = f.read().rstrip().strip("<>")
+
+    # Ensure message's format
     message = message.upper()
     message_enc = ""
+
+    # Map characters in message with characters in dictionary
     for i in message:
         if i in key:
             message_enc += key[i]
+        else:
+            print(i + " is not defined.")
+
     return message_enc
 
 
+# Keyfile must be in dictionary format
 def decrypt(file, key_file):
+    # Load key as dictionary
     key = json.load(open(key_file, 'r'))
+
+    # Create reversed key dictionary since cannot get key from value
     reverse_key = {}
     for k, v in key.items():
         reverse_key[v] = k
+
+    # Read message from file and remove '<>' characters
     with open(file, 'r') as f:
         message = f.read().rstrip().strip("<>")
 
+    # Ensure message's format
     message = message.upper()
 
+    # Map characters in message with characters in dictionary
     message_dec = ""
     for i in message:
         if i in reverse_key:
             message_dec += reverse_key[i]
+        else:
+            print(i + " is not defined.")
+
     return message_dec
 
 
-if sys.argv[1] == 'g':
+if sys.argv[1] == 'g':      # Generate key
     genkey(sys.argv[2])
-elif sys.argv[1] == 'f':
+elif sys.argv[1] == 'f':    # Additional mode for frequency checking
     print(checkfrequency(sys.argv[2]))
-elif sys.argv[1] == 'e':
+elif sys.argv[1] == 'e':    # Encrypt
     print(encrypt(sys.argv[2], sys.argv[3]))
-elif sys.argv[1] == 'd':
+elif sys.argv[1] == 'd':    # Decrypt
     print(decrypt(sys.argv[2], sys.argv[3]))
-else:
+else:                       # Mode validation
     print("Invalid input.")
 
